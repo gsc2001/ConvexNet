4754628c3bdc30a6ccb36aadf25525fb6508e1f3
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2022-03-01 00:18:00 +0530

    cycle mlp: add cifar10 to args

--- model_repos/CycleMLP/main.py
+++ model_repos/CycleMLP/main.py
@@ -170,7 +170,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19'],
+    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19', 'CIFAR10'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--mcloader', action='store_true', default=False, help='whether use mcloader')
     parser.add_argument('--inat-category', default='name',
