fb04646ca9a2083be98567903432ffe538fc7449
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2022-03-26 16:27:21 +0530

    new densenet code

---
+++ src/models/densenet/module.py
@@ -0,0 +1,42 @@
+from torch import nn, optim
+import pytorch_lightning as pl
+import torchmetrics
+from .model import DenseNet
+
+
+class DensenetModule(pl.LightningModule):
+    def __init__(self, growth_rate, block_config, num_init_features, **kwargs):
+        super(DensenetModule, self).__init__()
+
+        self.model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.train_acc = torchmetrics.Accuracy()
+        self.valid_acc = torchmetrics.Accuracy()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(self.parameters(), lr=0.1, weight_decay=1e-4, momentum=0.9)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, 30, 0.1)
+        return [optimizer], [lr_scheduler]
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        self.train_acc(y_hat, y)
+        loss = self.criterion(y_hat, y)
+
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        self.valid_acc(y_hat, y)
+        loss = self.criterion(y_hat, y)
+        self.log("valid_acc", self.valid_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("valid_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
---
+++ src/train.py
@@ -0,0 +1,32 @@
+"""
+The main train file
+    :author: gsc2001
+    :brief: File to train on ImageNet
+"""
+import argparse
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from pl_bolts.datamodules import ImagenetDataModule
+from models.densenet import DensenetModule
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", help="Data dir for imagenet", required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    imagenet = ImagenetDataModule(args.data_dir)
+    densenet = DensenetModule(32, (6, 12, 24, 16), 64)
+
+    logger = WandbLogger(project="ConvexNets")
+    trainer = pl.Trainer(max_epochs=90, logger=logger)
+    trainer.fit(densenet, imagenet)
+
+
+if __name__ == '__main__':
+    main()
