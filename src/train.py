de89a89ca3df600913c012a359ee19625e6acbbe
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2022-04-11 19:10:09 +0530

    add cifar10

--- src/train.py
+++ src/train.py
@@ -14,6 +14,7 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from data_modules.imagenet import ImagenetDataModule
+from pl_bolts.datamodules import CIFAR10DataModule
 from models.densenet import DensenetModule
 from models.ioc_densenet import IOCDensenetModule
 
@@ -24,6 +25,7 @@ def add_global_args(parent_parser: argparse.ArgumentParser):
     parser.add_argument("--batch-size", help="Training batch_size", default=64, type=int)
     parser.add_argument("--convex", action='store_true')
     parser.add_argument("--save_dir", required=True)
+    parser.add_argument("--dataset", default='imagenet')
     return parent_parser
 
 
@@ -51,8 +53,12 @@ def main():
 
     logger = WandbLogger(project="ConvexNets")
     trainer = pl.Trainer(max_epochs=30, logger=logger, gpus=1, callbacks=[checkpoint_callback])
-    imagenet = ImagenetDataModule(args.data_dir, batch_size=args.batch_size)
-    trainer.fit(densenet, imagenet)
+    if args.dataset == 'imagenet':
+        datamodule = ImagenetDataModule(args.data_dir, batch_size=args.batch_size)
+    else:
+        datamodule = CIFAR10DataModule(args.data_dir, batch_size=args.batch_size)
+
+    trainer.fit(densenet, datamodule=datamodule)
 
 
 if __name__ == '__main__':
